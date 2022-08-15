b16257a7c4a3cf68841182aa7e0351e0115398ac
ganjali89 <sadraganjali@gmail.com>
2022-08-15 15:31:38 +0430

    Debug and Delete The django source code

--- Nasiba/main/bin/ticket.py
+++ Nasiba/main/bin/ticket.py
@@ -12,9 +12,12 @@ django.setup()
 
 from main.models import Trakoneshs
 
+start = 3111
+i = 0
+
 under_test = requests.get(
     "https://newsepasa.na30ba.ir/v1/credit/merchant/transaction-list/",
-    params={'filter_id__gt': "3111", "filter_terminal__merchant__merchant_id": 1111188822,
+    params={'filter_id__gt': start, "filter_terminal__merchant__merchant_id": 1111188822,
             "page_size": 10, "order_by": "id"},
     headers={
         "Authorization": "Token client:xkMWWuLlnXdwusEJbjSk06kWSmHZ6fawp8nT7lmDSbsvs/07W8VuW9EToZtx1YxaSw1Ko2jttyTI5cVD"},
@@ -22,46 +25,58 @@ under_test = requests.get(
 
 under_json = under_test.json()
 under = under_json["results"]
-
-if under:
-    for i in under:
-        start = 3111
-        code1 = "please enter a your code!"
-        response = requests.get(
+if True:
+    for cd in under:
+        under_test = requests.get(
             "https://newsepasa.na30ba.ir/v1/credit/merchant/transaction-list/",
             params={'filter_id__gt': start, "filter_terminal__merchant__merchant_id": 1111188822,
-                    "page_size": 10, "order_by": "-id"},
+                    "page_size": 10, "order_by": "id"},
             headers={
                 "Authorization": "Token client:xkMWWuLlnXdwusEJbjSk06kWSmHZ6fawp8nT7lmDSbsvs/07W8VuW9EToZtx1YxaSw1Ko2jttyTI5cVD"},
         )
 
-        responsejson = response.json()
-        trx_id1 = responsejson["results"][0]["contract"]
+        under_json = under_test.json()
+        under = under_json["results"]
 
-        response2 = requests.get(
-            "https://newsepasa.na30ba.ir/v1/wallet/user/",
-            params={"filter_contract__id": 1891},
-            headers={
-                "Authorization": "Token client:xkMWWuLlnXdwusEJbjSk06kWSmHZ6fawp8nT7lmDSbsvs/07W8VuW9EToZtx1YxaSw1Ko2jttyTI5cVD"},
-        )
+        if under:
+            code1 = "enter your code"
+            response = requests.get(
+                "https://newsepasa.na30ba.ir/v1/credit/merchant/transaction-list/",
+                params={'filter_id__gt': start, "filter_terminal__merchant__merchant_id": 1111188822,
+                        "page_size": 10, "order_by": "-id"},
+                headers={
+                    "Authorization": "Token client:xkMWWuLlnXdwusEJbjSk06kWSmHZ6fawp8nT7lmDSbsvs/07W8VuW9EToZtx1YxaSw1Ko2jttyTI5cVD"},
+            )
 
-        response2json = response2.json()
-        cellphone1 = response2json["results"][0]["cellphone"]
 
-        response3 = requests.post(
-            "https://newsepasa.na30ba.ir/v1/campaign/send-sms/",
-            headers={
-                "Authorization": "Token client:xkMWWuLlnXdwusEJbjSk06kWSmHZ6fawp8nT7lmDSbsvs/07W8VuW9EToZtx1YxaSw1Ko2jttyTI5cVD"},
-            json={
-                "mobile": "09022562088",
-                "template": "cinemaTest",
-                "params": {"param1": code1},
-            },
-        )
+            trx_id1 = cd['contract']
 
-        response3json = response3.json()
-        campaign_id1 = response3json["campaign_id"]
-        start = start + 1
-        Trakoneshs.objects.create(
-            id1=start, code=code1, trx_id=trx_id1, cellphone=cellphone1, campaign_id=campaign_id1)
-        print("ok!")
+            response2 = requests.get(
+                "https://newsepasa.na30ba.ir/v1/wallet/user/",
+                params={"filter_contract__id": trx_id1},
+                headers={
+                    "Authorization": "Token client:xkMWWuLlnXdwusEJbjSk06kWSmHZ6fawp8nT7lmDSbsvs/07W8VuW9EToZtx1YxaSw1Ko2jttyTI5cVD"},
+            )
+
+            response2json = response2.json()
+            cellphone1 = response2json["results"][0]["cellphone"]
+
+            response3 = requests.post(
+                "https://newsepasa.na30ba.ir/v1/campaign/send-sms/",
+                headers={
+                    "Authorization": "Token client:xkMWWuLlnXdwusEJbjSk06kWSmHZ6fawp8nT7lmDSbsvs/07W8VuW9EToZtx1YxaSw1Ko2jttyTI5cVD"},
+                json={
+                    "mobile": cellphone1,
+                    "template": "cinemaTest",
+                    "params": {"param1": code1},
+                },
+            )
+
+            response3json = response3.json()
+            campaign_id1 = response3json["campaign_id"]
+            start = start + 1
+            i = i + 1
+            Trakoneshs.objects.create(id1=start, code=code1, trx_id=trx_id1,cellphone=cellphone1, campaign_id=campaign_id1)
+            print("ok!")
+        else:
+            break
